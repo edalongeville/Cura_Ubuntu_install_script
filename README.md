bd4bad03b9ab31e46f664291bcfdc2a19ed269ec
Eric Dalongeville <22616866+edalongeville@users.noreply.github.com>
2020-01-31 22:21:52 +0100

    Initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Cura_Ubuntu_install_script
-This script properly installs Cura on Ubuntu 18.04
+# Cura_install_script
+This script will install Cura (with icons and .desktop link) on Ubuntu 18.04
+
+## This is intended to be used on Ubuntu 18.04.
+
+# What it does
+- Deploys Cura with your other apps (in /usr/bin)
+- Creates a .desktop file, so you can access Cura from Gnome, like any other app
+- Deploys the icon, required for the above .desktop link
+
+# Instructions
+1. Download/Clone this repo
+2. Download the linux vesion of Cura from https://ultimaker.com/software/ultimaker-cura
+3. Place the downloaded AppImage in the same directory as the install script
+4. Run the install script (**root access will be required**). If required, add exec permission with *chmod +x*.
+
+Note: You may have to restart your session for the icon to be properly displayed in gnome
+
---
+++ cura_install_Ubuntu.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# This script installs Cura on Ubuntu 18.04
+
+FILE="Ultimaker_Cura-4.4.1.AppImage"
+ICON="cura.png"
+DESKFILE="cura.desktop"
+INSTALL_DIR="/usr/bin/cura"
+ICONS_DIR="/usr/share/icons/hicolor"
+DESKFILES_DIR="/usr/share/applications"
+
+# Ensure Cura archive is here
+if test -f "$FILE"; then
+	echo "Cura appimage found"
+else
+	echo "Please download Cura AppImage from their website and place the archive in the same folder as this script"
+	echo "https://ultimaker.com/software/ultimaker-cura"
+	exit 1
+fi
+
+# Ensure icon is here
+if ! test -f "$ICON"; then
+	echo "Icon not found! $ICON should be in the same folder as this script"
+	exit 1
+fi
+
+# Extract Cura to install dir
+sudo mkdir $INSTALL_DIR
+sudo mv $FILE $INSTALL_DIR
+sudo chmod +x $INSTALL_DIR/$FILE
+
+# Deploy icons
+## 32x32
+sudo convert -resize 32x32 $ICON $ICONS_DIR/32x32/apps/$ICON
+## 48x48
+sudo convert -resize 48x48 $ICON $ICONS_DIR/48x48/apps/$ICON
+## 64x64
+sudo convert -resize 64x64 $ICON $ICONS_DIR/64x64/apps/$ICON
+## 128x128
+sudo cp $ICON $ICONS_DIR/128x128/apps/$ICON
+
+# Deploy desktop file (Gnome menu entry)
+sudo cp $DESKFILE $DESKFILES_DIR/$DESKFILE
+
+echo "Cura installation complete!"
