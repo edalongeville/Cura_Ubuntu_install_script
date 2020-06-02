ee7dd99555d3d0b9b8e1edc2cc91f14d570f34a5
Eric Dalongeville <22616866+edalongeville@users.noreply.github.com>
2020-06-02 10:24:07 +0200

    Version bump

--- cura_install_Ubuntu.sh
+++ cura_install_Ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script installs Cura on Ubuntu 18.04
 
-FILE="Ultimaker_Cura-4.4.1.AppImage"
+FILE="Ultimaker_Cura-4.6.1.AppImage"
 ICON="cura.png"
 DESKFILE="cura.desktop"
 INSTALL_DIR="/usr/bin/cura"
